e621f59ce8e915aae82ef1af4db946fdcf26b987
kelechiokpani <mremmatex@gmail.com>
2026-04-09 05:03:09 +0100

    fixing error and ui modification

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import '@/app/lib/models/user';
+import '@/app/lib/models/product';
+
 
 const MONGODB_URI:any = process.env.MONGODB_URI || "mongodb+srv://Colycia:9dztgdgub7lKfr5V@colycia.qwe1apn.mongodb.net/?appName=Colycia";
 
